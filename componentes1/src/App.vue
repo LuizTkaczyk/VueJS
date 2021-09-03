1e67cfc951a86a293dabc2f8e273ac8869824fc5
LuizTkaczyk <luiz8907@hotmail.com>
2021-09-02 21:31:30 -0300

    Components

---
+++ componentes1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+
+     <AppHeader/>
+  </div>
+ 
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+export default {
+  name: "App",
+  components: {
+    AppHeader
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Rajdhani&display=swap");
+</style>
